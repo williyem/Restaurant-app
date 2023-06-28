bd7731fad6fa1e310168e7aeba5e541d88876af6
williyem <hinnehwilliams@gmail.com>
2023-06-28 22:01:43 +0000

    fix: navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { NavBar } from "@/components";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import { Footer } from "@/components";
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Delivery App',
-  description: 'Food delivery app ',
-}
+  title: "Delivery App",
+  description: "Food delivery app ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
---
+++ utils/ui-data.tsx
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    id: 0,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 1,
+    name: "Restaurants",
+    link: "/restaurants",
+  },
+  {
+    id: 2,
+    name: "My orders",
+    link: "/orders",
+  },
+];
